84af885dd8ab842d582a1a19575a94d245417fee
klwortman <klwortman@192.168.1.3>
2016-04-14 17:26:41 -0500

    Made various changes

--- www/js/uploadImage.js
+++ www/js/uploadImage.js
@@ -76,3 +76,24 @@
         console.log("upload error source " + error.source);
         console.log("upload error target " + error.target);
     }
+    
+    /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
